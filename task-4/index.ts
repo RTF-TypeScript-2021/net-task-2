5b69af40eef8ccb4f2c8b0faa758cc90baccaddf
Sychqh <sychqh@gmail.com>
2021-12-20 20:38:05 +0500

    Сделал 4 задание

--- task-4/index.ts
+++ task-4/index.ts
@@ -4,7 +4,22 @@
  * Так как мы получаем факты о котиках на непонятном языке,
  * давай переведем их на русский))
  */
+import {TranslateResponseModel, CatFactResponseModel, TranslateRequestModel} from "../task-2";
+import fetch from "node-fetch";
 
-export function translateCatFact(fact: CatFactResponseModel): Promise<TranslateResponseModel>{
-
+export async function translateCatFact(fact: CatFactResponseModel): Promise<TranslateResponseModel> {
+    const url = 'https://trans.zillyhuhn.com/translate';
+    const request: TranslateRequestModel = {
+        method: "POST",
+        body: JSON.stringify({
+            q: fact.fact,
+            source: "en",
+            target: "ru",
+            format: "text"
+        }),
+        headers: {"Content-Type": "application/json"}
+    };
+    const response = await fetch(url, request);
+    
+    return await response.json() as TranslateResponseModel;
 }
