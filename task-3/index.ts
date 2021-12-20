d73437858f015f7e8928f2003986aac9b6b48cb2
Sychqh <sychqh@gmail.com>
2021-12-20 19:59:03 +0500

    Сделал 3 задание

--- task-3/index.ts
+++ task-3/index.ts
@@ -3,7 +3,11 @@
  *
  * С помощью предыдущих моделей запроси рандомный факт о котиках
  */
+import {CatFactResponseModel} from "../task-2";
+import fetch from "node-fetch";
 
-export function getCatFact(): Promise<CatFactResponseModel>{
-
+export function getCatFact(): Promise<CatFactResponseModel> {
+    const url = "https://catfact.ninja/fact";
+    
+    return await fetch(url).then(response => response.json() as CatFactResponseModel);
 }
