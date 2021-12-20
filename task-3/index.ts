bf5cc4810ef1b9f095a7990b37aecabc06edcf7c
Darctys <yltimatym2002@maiil.ru>
2021-12-20 19:14:46 +0500

    Сычев Тимофей

--- task-3/index.ts
+++ task-3/index.ts
@@ -4,6 +4,13 @@
  * С помощью предыдущих моделей запроси рандомный факт о котиках
  */
 
-export function getCatFact(): Promise<CatFactResponseModel>{
+import fetch from "node-fetch";
+import {CatFactResponseModel} from "../task-2";
 
+export async function getCatFact(): Promise<CatFactResponseModel> {
+    const answer = await fetch("https://catfact.ninja/fact");
+    if (answer.ok){
+        return await answer.json() as CatFactResponseModel
+    }
+    throw new Error();
 }
--- task-4/index.ts
+++ task-4/index.ts
@@ -4,7 +4,22 @@
  * Так как мы получаем факты о котиках на непонятном языке,
  * давай переведем их на русский))
  */
+import {CatFactResponseModel, TranslateResponseModel} from "../task-2";
+import fetch from "node-fetch";
 
-export function translateCatFact(fact: CatFactResponseModel): Promise<TranslateResponseModel>{
-
+export async function translateCatFact(fact: CatFactResponseModel): Promise<TranslateResponseModel>{
+    const answer = await fetch("https://trans.zillyhuhn.com/translate", {
+        method: "POST",
+        body: JSON.stringify({
+            q: fact.fact,
+            source: "en",
+            target: "ru",
+            format: "text"
+        }),
+        headers: { "Content-Type": "application/json" }
+    });
+    if(answer.ok) {
+        return await answer.json() as TranslateResponseModel;
+    }
+    throw new Error();
 }
