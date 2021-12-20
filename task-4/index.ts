7a6c1d623c52d9c3ca1710300037af95da2548ca
Angelina Shkatova <shkatova.angel@yandex.ru>
2021-12-20 15:18:10 +0500

    Task 4 is solved

--- task-4/index.ts
+++ task-4/index.ts
@@ -4,7 +4,20 @@
  * Так как мы получаем факты о котиках на непонятном языке,
  * давай переведем их на русский))
  */
+import {CatFactResponseModel, TranslateResponseModel} from "../task-2";
+import fetch from "node-fetch";
 
-export function translateCatFact(fact: CatFactResponseModel): Promise<TranslateResponseModel>{
+export async function translateCatFact(fact: CatFactResponseModel): Promise<TranslateResponseModel>{
+    const res = await fetch("https://trans.zillyhuhn.com/translate", {
+        method: "POST",
+        body: JSON.stringify({
+            q: fact.fact,
+            source: "en",
+            target: "ru",
+            format: "text"
+        }),
+        headers: { "Content-Type": "application/json" }
+    });
 
+    return await res.json() as TranslateResponseModel;
 }
