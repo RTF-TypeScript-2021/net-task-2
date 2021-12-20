dc400cc4a326dfb9a4cf899efff5dafef455ca11
Sychqh <sychqh@gmail.com>
2021-12-20 19:57:32 +0500

    Сделал 1 задание

--- task-1/index.ts
+++ task-1/index.ts
@@ -1,15 +1,16 @@
-import fetch from "node-fetch";
 /**
  * Задание - 1
  * Напишите функцию, которая проверяет наличие пользователя на площадке Github используя fetch
  * Данные что приходят с сервера нужно типизировать.
  */
-
+import fetch from "node-fetch";
 
 /**
  * Проверяет существование пользователя на GH
  * @param username - юзернейм
  */
-export function isUserExist(username: string): Promise<boolean>{
-
+export async function isUserExist(username: string): Promise<boolean> {
+    const url = "https://api.github.com/users/${username}";
+    
+    return await fetch(url).then(response => response.ok);
 }
