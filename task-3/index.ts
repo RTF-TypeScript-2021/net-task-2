bbbd5621cc0177fa16af860fa5466378f0e15e71
Angelina Shkatova <shkatova.angel@yandex.ru>
2021-12-20 14:53:24 +0500

    Task 3 is solved

--- task-3/index.ts
+++ task-3/index.ts
@@ -3,7 +3,12 @@
  *
  * С помощью предыдущих моделей запроси рандомный факт о котиках
  */
+import fetch from "node-fetch";
+import {CatFactResponseModel} from "../task-2";
 
-export function getCatFact(): Promise<CatFactResponseModel>{
+export async function getCatFact(): Promise<CatFactResponseModel> {
+    const answer = await fetch("https://catfact.ninja/fact?max_length=100");
 
+    return await answer.json() as CatFactResponseModel;
 }
+
